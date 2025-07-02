b3b56a3430420a63b4d4c42adfd055420da0190b
Ilya Kreymer <ikreymer@users.noreply.github.com>
2025-07-02 00:18:11 -0700

    Update init.js with site, WACZ

--- init.js
+++ init.js
@@ -1,2 +1,2 @@
 // Fill in WACZ / Multi WACZ JSON and origin here
-// init("https://example.com/path/to/my/{file.wacz,collection.json}", "https://my-archived-site.example.com", "<optional timestamp>");
+init("https://sfo2.digitaloceanspaces.com/dh-preserve/webarchives/mmm-pubpub.wacz", "https://mmm.pubpub.org/");
